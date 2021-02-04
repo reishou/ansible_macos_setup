e592cabb5fe080ac7279c133763a3573e533617f
Reishou <reishou90@gmail.com>
2021-02-05 00:04:27 +0700

    add some simple file

---
+++ README.md
@@ -0,0 +1,1 @@
+# ansible_macos_setup
--- start.sh
+++ start.sh
@@ -13,14 +13,14 @@ if [ "$check_homebrew" == "" ]; then
 fi
 
 brew ls --versions ansible || brew install ansible
-brew outdated ansible || brew install ansible
+brew outdated ansible || brew upgrade ansible
 
 brew ls --versions git || brew install git
 brew outdated git || brew upgrade git
 
 dir="/tmp/$RANDOM"
 mkdir "$dir"
-git clone git@github.com:reishou/ansible_macos_setupw.git $dir
+git clone git@github.com:reishou/ansible_macos_setup.git $dir
 
 if [ ! "$(ls -A "$dir")" ]; then
 	# @TODO: check ansible file exist
